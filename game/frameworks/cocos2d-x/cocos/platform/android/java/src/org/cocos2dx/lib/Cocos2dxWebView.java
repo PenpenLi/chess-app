88fb64cd6f137e4885aa7596a97a306e45729daa
haha <haha@gmail.com>
2020-01-31 14:18:50 +0800

    add cocos2d-x

---
+++ game/frameworks/cocos2d-x/cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxWebView.java
@@ -0,0 +1,199 @@
+package org.cocos2dx.lib;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+import android.view.Gravity;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+
+class ShouldStartLoadingWorker implements Runnable {
+    private CountDownLatch mLatch;
+    private boolean[] mResult;
+    private final int mViewTag;
+    private final String mUrlString;
+
+    ShouldStartLoadingWorker(CountDownLatch latch, boolean[] result, int viewTag, String urlString) {
+        this.mLatch = latch;
+        this.mResult = result;
+        this.mViewTag = viewTag;
+        this.mUrlString = urlString;
+    }
+
+    @Override
+    public void run() {
+        this.mResult[0] = Cocos2dxWebViewHelper._shouldStartLoading(mViewTag, mUrlString);
+        this.mLatch.countDown(); // notify that result is ready
+    }
+}
+
+public class Cocos2dxWebView extends WebView {
+    private static final String TAG = Cocos2dxWebViewHelper.class.getSimpleName();
+
+    private int mViewTag;
+    private String mJSScheme;
+
+    public Cocos2dxWebView(Context context) {
+        this(context, -1);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    public Cocos2dxWebView(Context context, int viewTag) {
+        super(context);
+        this.mViewTag = viewTag;
+        this.mJSScheme = "";
+
+        this.setFocusable(true);
+        this.setFocusableInTouchMode(true);
+
+        this.getSettings().setSupportZoom(false);
+        this.getSettings().setDomStorageEnabled(true);
+        this.getSettings().setDefaultTextEncodingName("UTF-8");
+        this.getSettings().setJavaScriptEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            this.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
+            this.getSettings().setAllowFileAccessFromFileURLs(true);
+            this.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        }
+
+        // `searchBoxJavaBridge_` has big security risk. http://jvn.jp/en/jp/JVN53768697
+        try {
+            Method method = this.getClass().getMethod("removeJavascriptInterface", new Class[]{String.class});
+            method.invoke(this, "searchBoxJavaBridge_");
+        } catch (Exception e) {
+            Log.d(TAG, "This API level do not support `removeJavascriptInterface`");
+        }
+
+        this.setWebViewClient(new Cocos2dxWebViewClient());
+        this.setWebChromeClient(new MyWebChromeClient());
+    }
+
+
+    /**
+     * 内部类
+     */
+    class MyWebChromeClient extends WebChromeClient {
+        //For Android 3.0-
+        public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+            Log.d(TAG, "openFileChooser: ");
+            ((Cocos2dxActivity)Cocos2dxActivity.getContext()).setUploadMessage(uploadMsg);
+            ((Cocos2dxActivity)Cocos2dxActivity.getContext()).pickImage(1);
+        }
+
+        // For Android 3.0+
+        public void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+            Log.d(TAG, "openFileChooser: ");
+            ((Cocos2dxActivity)Cocos2dxActivity.getContext()).setUploadMessage(uploadMsg);
+            ((Cocos2dxActivity)Cocos2dxActivity.getContext()).pickImage(1);
+        }
+
+        //For Android 4.1
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+            Log.d(TAG, "openFileChooser: ");
+            ((Cocos2dxActivity)Cocos2dxActivity.getContext()).setUploadMessage(uploadMsg);
+            ((Cocos2dxActivity)Cocos2dxActivity.getContext()).pickImage(1);
+        }
+
+        //For Android 5.0+
+        @Override
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+            Log.d(TAG, "onShowFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)");
+            ((Cocos2dxActivity)Cocos2dxActivity.getContext()).setUploadFilePathCallback(filePathCallback);
+            ((Cocos2dxActivity)Cocos2dxActivity.getContext()).pickImage(1);
+            return true;
+        }
+    }
+
+    public void setJavascriptInterfaceScheme(String scheme) {
+        this.mJSScheme = scheme != null ? scheme : "";
+    }
+
+    public void setScalesPageToFit(boolean scalesPageToFit) {
+        this.getSettings().setSupportZoom(scalesPageToFit);
+    }
+
+    class Cocos2dxWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, final String urlString) {
+            Cocos2dxActivity activity = (Cocos2dxActivity)getContext();
+
+            try {
+                URI uri = URI.create(urlString);
+                if (uri != null && uri.getScheme().equals(mJSScheme)) {
+                    activity.runOnGLThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Cocos2dxWebViewHelper._onJsCallback(mViewTag, urlString);
+                        }
+                    });
+                    return true;
+                }
+            } catch (Exception e) {
+                Log.d(TAG, "Failed to create URI from url");
+            }
+
+            boolean[] result = new boolean[] { true };
+            CountDownLatch latch = new CountDownLatch(1);
+
+            // run worker on cocos thread
+            activity.runOnGLThread(new ShouldStartLoadingWorker(latch, result, mViewTag, urlString));
+
+            // wait for result from cocos thread
+            try {
+                latch.await();
+            } catch (InterruptedException ex) {
+                Log.d(TAG, "'shouldOverrideUrlLoading' failed");
+            }
+
+            return result[0];
+        }
+
+        @Override
+        public void onPageFinished(WebView view, final String url) {
+            super.onPageFinished(view, url);
+            Cocos2dxActivity activity = (Cocos2dxActivity)getContext();
+            activity.runOnGLThread(new Runnable() {
+                @Override
+                public void run() {
+                    Cocos2dxWebViewHelper._didFinishLoading(mViewTag, url);
+                }
+            });
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, final String failingUrl) {
+            super.onReceivedError(view, errorCode, description, failingUrl);
+            Cocos2dxActivity activity = (Cocos2dxActivity)getContext();
+            activity.runOnGLThread(new Runnable() {
+                @Override
+                public void run() {
+                    Cocos2dxWebViewHelper._didFailLoading(mViewTag, failingUrl);
+                }
+            });
+        }
+    }
+
+    public void setWebViewRect(int left, int top, int maxWidth, int maxHeight) {
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftMargin = left;
+        layoutParams.topMargin = top;
+        layoutParams.width = maxWidth;
+        layoutParams.height = maxHeight;
+        layoutParams.gravity = Gravity.TOP | Gravity.LEFT;
+        this.setLayoutParams(layoutParams);
+    }
+}
